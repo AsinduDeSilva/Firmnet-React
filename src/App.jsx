f69a1a23f57f84d88cb518fef133c4ba4a57c9ce
Ashika Dandeniya <148692377+AshikaDandeniya@users.noreply.github.com>
2025-03-19 16:48:17 +0530

    performance added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <div className="flex">
-      <Sidebar />
-      <div className="flex-1 p-10">
-      </div>
-    </div>
+    <>
+    </>
   );
 }
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ const Sidebar = () => {
     <div className="bg-white text-black w-64 space-y-6 py-7 px-2 absolute inset-y-0 left-0 transform -translate-x-full md:relative md:translate-x-0 transition duration-100 ease-in-out">
       <div className="text-black flex items-center space-x-2 px-4">
       <div className="flex justify-center items-center">
-        <img src="/logo.png" alt="Logo"className="h-50 w-50" />
+        <img src="/logo1.png" alt="Logo"className="h-40 w-60" />
       </div>
       </div>
       <nav className="space-y-8">
