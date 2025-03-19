506a37f4d56d31c89dfd2309267aec86511d8e52
ArunodiAAL <>
2025-03-19 20:06:14 +0530

    firmware page Updated..

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Sidebar from './components/Sidebar';
 import Firmware from './components/Firmware';
 import Devices from './components/Devices'
-
+import Performance from './components/Perfomance';
+import LoginForm from './components/Signin';
 function App() {
   return (
     <div className="flex">
       <Sidebar />
-      <div className="flex-1 p-10">
-        <Devices />
+      <div className="flex-1 pt-10">
+        <Firmware />
       </div>
     </div>
   );
