46503918ac9de83a780cc1da96a757cf03fc7918
DewmiS <dewmisamadhipathiranage@gmail.com>
2025-05-10 14:02:04 +0530

    UI updated

--- src/components/AddDeviceModal.jsx
+++ src/components/AddDeviceModal.jsx
@@ -12,7 +12,7 @@ const AddDeviceModal = ({ isOpen, onClose, onSubmit }) => {
 
                 <form onSubmit={onSubmit} className="space-y-4">
                     <div>
-                        <label className="block text-sm font-medium text-[#ccccd6]">Device ID</label>
+                        <label className="block text-sm font-medium text-[#ccccd6]">Name</label>
                         <input type="text" className="mt-1 p-2 w-full border rounded-md bg-[#8b8b97]" required />
                     </div>
 
