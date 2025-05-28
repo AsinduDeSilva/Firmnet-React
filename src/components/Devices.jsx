bf1791647fcd3ff70ef44c5a14ab1340ef056975
AshikaDandeniya <ashikadandeniya@gmail.com>
2025-05-28 20:22:46 +0530

    Updated

--- src/components/Devices.jsx
+++ src/components/Devices.jsx
@@ -8,6 +8,8 @@ import {
   TableHead,
   TablePagination,
   TableRow,
+  Alert,
+  Slide,
 } from "@mui/material";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 import EditIcon from "@mui/icons-material/Edit";
@@ -30,6 +32,7 @@ const Devices = () => {
   const [isAddDdeviceModalOpen, setAddDdeviceModalOpen] = useState(false);
   const [isUpdateDdeviceModalOpen, setUpdateDdeviceModalOpen] = useState(false);
   const [selectedDevice, setSelectedDevice] = useState(0);
+  const [deleteSuccess, setDeleteSuccess] = useState(false); 
 
   const handleChangePage = (event, newPage) => setPage(newPage);
   const handleChangeRowsPerPage = (event) => {
@@ -38,118 +41,154 @@ const Devices = () => {
   };
 
   const handleDelete = (deviceId) => {
-    if(!confirm("Are you sure want to delete this device?")){
-      return;
-    }
+    if (!confirm("Are you sure want to delete this device?")) return;
     deleteDevice(deviceId);
   };
 
-  useEffect(()=> {
+  useEffect(() => {
     loadDevices();
-  },[])
+  }, []);
 
   const loadDevices = () => {
     fetch(`${backendAddress}/api/v1/device`)
-    .then((response) => response.json())
-    .then((data) => {
-      setRows(data)
-    })
-  }
+      .then((response) => response.json())
+      .then((data) => setRows(data));
+  };
 
   const deleteDevice = (deviceId) => {
-    fetch(`${backendAddress}/api/v1/device/${deviceId}`,{
-      method: "DELETE"
+    fetch(`${backendAddress}/api/v1/device/${deviceId}`, {
+      method: "DELETE",
     })
-    .then((response) => response.json())
-    .then((data) => {
-      if(data.success){
-        loadDevices()
-        alert("Device deleted successfully.")
-      }
-    })
-  }
-
-
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          loadDevices();
+          setDeleteSuccess(true); 
+          setTimeout(() => setDeleteSuccess(false), 3000); 
+        }
+      });
+  };
 
   return (
     <>
+     
+      {deleteSuccess && (
+        <Slide direction="down" in={deleteSuccess} mountOnEnter unmountOnExit>
+          <div className="absolute top-4 z-50 w-full flex justify-center">
+            <Alert severity="error" className="w-1/2 text-center">
+              Device deleted successfully.
+            </Alert>
+          </div>
+        </Slide>
+      )}
+
       <div className="flex flex-col items-center min-h-screen bg-[#15151f]">
         <h2 className="text-4xl font-bold my-10 text-[#d3d5de] w-[95%]">
           DEVICES
         </h2>
         <div className="flex w-[95%] justify-end pb-5">
-            <button
-              className="px-8 py-2 rounded-md bg-[#1e1e2b] border-[#4b9eda] border-2 text-[#4b9eda] hover:bg-[#252c42] hover:text-[#4b9eda]  hover:cursor-pointer"
-              onClick={() => setAddDdeviceModalOpen(true)}
-            >    
-                + Add Device
-            </button>
+          <button
+            className="px-8 py-2 rounded-md bg-[#1e1e2b] border-[#4b9eda] border-2 text-[#4b9eda] hover:bg-[#252c42] hover:text-[#4b9eda] hover:cursor-pointer"
+            onClick={() => setAddDdeviceModalOpen(true)}
+          >
+            + Add Device
+          </button>
         </div>
-        <Paper sx={{ width: "95%", overflow: "hidden", backgroundColor: "#1e1e2b", borderRadius: 3 }}>
-            <TableContainer sx={{ maxHeight: 400 }}>
-                <Table stickyHeader>
-                  <TableHead sx={{ backgroundColor: "#1e1e2b", }}>
-                      <TableRow>
-                        {columns.map((column) => (
-                            <TableCell
-                              key={column.id}
-                              align={column.align}     
-                              sx={{ color: "#d3d5de", backgroundColor: "#1e1e2b",fontWeight:"bold", fontSize:"16px", borderBottom:"1px solid #8b8b97"}}
-                            >
-                              {column.label}
-                            </TableCell>
-                        ))}
-                      </TableRow>
-                  </TableHead>
-                  <TableBody sx={{ backgroundColor: "#1e1e2b", borderBottom:"1px solid #8b8b97" }}>
-                      {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => (
-                          <TableRow hover key={row.id}>
-                            {columns.map((column) => {
-                                const value = row[column.id];
-                                return (
-                                  <TableCell
-                                      key={column.id}
-                                      align={column.align}
-                                      sx={{ color: "#d3d5de", backgroundColor: "#1e1e2b", borderBottom:"1px solid #8b8b97"}}
-                                  >
-                                      {column.id === "actions" ? (
-                                        <div className="flex justify-center space-x-3">
-                                            <CloudUploadIcon className="text-[#d3d5de] cursor-pointer" />
-                                            <EditIcon 
-                                              className="text-[#d3d5de] cursor-pointer" 
-                                              onClick={() => {setUpdateDdeviceModalOpen(true); setSelectedDevice(row.id)}}
-                                            />
-                                            <DeleteIcon
-                                              className="text-[#d3d5de] cursor-pointer"
-                                              onClick={() => handleDelete(row.id)}
-                                            />
-                                        </div>
-                                      ) : (
-                                        value
-                                      )}
-                                  </TableCell>
-                                );
-                            })}
-                          </TableRow>
-                      ))}
-                  </TableBody>
-                </Table>
-            </TableContainer>
 
-            <TablePagination
-              rowsPerPageOptions={[5, 10, 25]}
-              component="div"
-              count={rows.length}
-              rowsPerPage={rowsPerPage}
-              page={page}
-              onPageChange={handleChangePage}
-              onRowsPerPageChange={handleChangeRowsPerPage}
-              sx={{color: "#d3d5de"}}
-            />
+        <Paper
+          sx={{
+            width: "95%",
+            overflow: "hidden",
+            backgroundColor: "#1e1e2b",
+            borderRadius: 3,
+          }}
+        >
+          <TableContainer sx={{ maxHeight: 400 }}>
+            <Table stickyHeader>
+              <TableHead>
+                <TableRow>
+                  {columns.map((column) => (
+                    <TableCell
+                      key={column.id}
+                      align={column.align}
+                      sx={{
+                        color: "#d3d5de",
+                        backgroundColor: "#1e1e2b",
+                        fontWeight: "bold",
+                        fontSize: "16px",
+                        borderBottom: "1px solid #8b8b97",
+                      }}
+                    >
+                      {column.label}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((row) => (
+                    <TableRow hover key={row.id}>
+                      {columns.map((column) => {
+                        const value = row[column.id];
+                        return (
+                          <TableCell
+                            key={column.id}
+                            align={column.align}
+                            sx={{
+                              color: "#d3d5de",
+                              backgroundColor: "#1e1e2b",
+                              borderBottom: "1px solid #8b8b97",
+                            }}
+                          >
+                            {column.id === "actions" ? (
+                              <div className="flex justify-center space-x-3">
+                                <CloudUploadIcon className="text-[#d3d5de] cursor-pointer" />
+                                <EditIcon
+                                  className="text-[#d3d5de] cursor-pointer"
+                                  onClick={() => {
+                                    setUpdateDdeviceModalOpen(true);
+                                    setSelectedDevice(row.id);
+                                  }}
+                                />
+                                <DeleteIcon
+                                  className="text-[#d3d5de] cursor-pointer"
+                                  onClick={() => handleDelete(row.id)}
+                                />
+                              </div>
+                            ) : (
+                              value
+                            )}
+                          </TableCell>
+                        );
+                      })}
+                    </TableRow>
+                  ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 25]}
+            component="div"
+            count={rows.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+            sx={{ color: "#d3d5de" }}
+          />
         </Paper>
 
-        <AddDeviceModal isOpen={isAddDdeviceModalOpen} onClose={() => setAddDdeviceModalOpen(false)} />
-        <UpdateDeviceModal isOpen={isUpdateDdeviceModalOpen} onClose={()=>setUpdateDdeviceModalOpen(false)} selectedDevice={selectedDevice}/>
+        <AddDeviceModal
+          isOpen={isAddDdeviceModalOpen}
+          onClose={() => setAddDdeviceModalOpen(false)}
+        />
+        <UpdateDeviceModal
+          isOpen={isUpdateDdeviceModalOpen}
+          onClose={() => setUpdateDdeviceModalOpen(false)}
+          selectedDevice={selectedDevice}
+        />
       </div>
     </>
   );
--- src/components/UpdateDeviceModal.jsx
+++ src/components/UpdateDeviceModal.jsx
@@ -1,102 +1,149 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import { Alert, Slide } from '@mui/material';
 import { backendAddress } from '../constants/BackendInfo';
 
 const UpdateDeviceModal = ({ isOpen, onClose, selectedDevice }) => {
-    if (!isOpen) return null;
-  
-    const[deviceName, setDeviceName] = useState("");
-    const[username, setUsername] = useState("");
-    const[password, setPassword] = useState("");
-    const[ipAddress, setIpAddress] = useState("");
+  if (!isOpen) return null;
 
-    const btnUpdateDeviceOnClick = () => {
-        if (!isValidated()) return;
-        
-        fetch(`${backendAddress}/api/v1/device/${selectedDevice}`, {
-            method: "PUT",
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            },
-            body: JSON.stringify({
-                name: deviceName,
-                username: username,
-                password: password,
-                ipAddress: ipAddress,
-            }),
-        })
-        .then((response) => response.json())
-        .then(data => {
-            if(data.success){
-                alert("Device updated successfully.")
-            }
-        });
-    }
+  const [deviceName, setDeviceName] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [ipAddress, setIpAddress] = useState('');
+  const [updateSuccess, setUpdateSuccess] = useState(false);
 
-    const isValidated = () => {
-        return true;
-    }
+  const btnUpdateDeviceOnClick = (e) => {
+    e.preventDefault();
+    if (!isValidated()) return;
 
-    const loadDeviceDeatils = () => {
-        fetch(`${backendAddress}/api/v1/device/${selectedDevice}`)
-        .then((response) => response.json())
-        .then((data) => {
-            if(data.success){
-                setDeviceName(data.data.name)
-                setUsername(data.data.username)
-                setPassword(data.data.password)
-                setIpAddress(data.data.ipAddress)
-            }
-        })
-    }
+    fetch(`${backendAddress}/api/v1/device/${selectedDevice}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify({
+        name: deviceName,
+        username: username,
+        password: password,
+        ipAddress: ipAddress,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          setUpdateSuccess(true);
+          setTimeout(() => {
+            setUpdateSuccess(false);
+            onClose();
+          }, 3000);
+        }
+      });
+  };
 
-    useEffect(() => {
-        loadDeviceDeatils();
-    }, []);
+  const isValidated = () => {
+    return true; 
+  };
 
-    return (
-        <>
-            <div className="fixed inset-0 bg-opacity-40 backdrop-blur-md flex justify-center items-center z-10">
-                <div className="p-6 rounded-lg shadow-lg w-160 bg-[#1e1e2b]">
-                    <h2 className="text-xl font-bold mb-4 text-[#d3d5de]">UPDATE DEVICE</h2>
+  const loadDeviceDeatils = () => {
+    fetch(`${backendAddress}/api/v1/device/${selectedDevice}`)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          setDeviceName(data.data.name);
+          setUsername(data.data.username);
+          setPassword(data.data.password);
+          setIpAddress(data.data.ipAddress);
+        }
+      });
+  };
 
-                    <form onSubmit={btnUpdateDeviceOnClick} className="space-y-4">
-                        <div>
-                            <label className="block text-[#ccccd6]">Name</label>
-                            <input value={deviceName} onChange={e => setDeviceName(e.target.value)} type="text" className="mt-1 p-2 w-full border rounded-md bg-[#d3d5de]" required />
-                        </div>
+  useEffect(() => {
+    loadDeviceDeatils();
+  }, [selectedDevice]);
 
-                        <div>
-                            <label className="block text-[#ccccd6]">Username</label>
-                            <input value={username} onChange={e => setUsername(e.target.value)} type="text" className="mt-1 p-2 w-full border rounded-md bg-[#d3d5de]" required />
-                        </div>
+  return (
+    <>
+      {updateSuccess && (
+        <Slide direction="down" in={updateSuccess} mountOnEnter unmountOnExit>
+          <div className="absolute top-4 z-50 w-full flex justify-center">
+            <Alert severity="info" className="w-1/2 text-center">
+              Device Updated Successfully!
+            </Alert>
+          </div>
+        </Slide>
+      )}
 
-                        <div>
-                            <label className="block text-[#ccccd6]">Password</label>
-                            <input value={password} onChange={e => setPassword(e.target.value)} type="password" className="mt-1 p-2 w-full border rounded-md bg-[#d3d5de]" required />
-                        </div>
+      <div className="fixed inset-0 bg-opacity-40 backdrop-blur-md flex justify-center items-center z-10">
+        <div className="p-6 rounded-lg shadow-lg w-160 bg-[#1e1e2b]">
+          <h2 className="text-xl font-bold mb-4 text-[#d3d5de]">UPDATE DEVICE</h2>
 
-                        <div>
-                            <label className="block text-[#ccccd6]">IP Address</label>
-                            <input value={ipAddress} onChange={e => setIpAddress(e.target.value)} type="text" className="mt-1 p-2 w-full border rounded-md bg-[#d3d5de]" required />
-                        </div>
-
-                        <div className="flex justify-end space-x-2">
-                            <button type="button" onClick={onClose} className="px-6 py-2 rounded-md bg-[#1e1e2b] border-[#4b9eda] border-2 
-                                text-[#4b9eda] hover:bg-[#252c42] hover:text-[#4b9eda]  hover:cursor-pointer">
-                                Cancel
-                            </button>
-                    
-                            <button type="submit" className="px-8 py-2 rounded-md bg-[#1e1e2b] border-[#4b9eda] border-2 
-                                text-[#4b9eda] hover:bg-[#252c42] hover:text-[#4b9eda]  hover:cursor-pointer">
-                                Update
-                            </button>
-                        </div>
-                    </form>
-                </div>
+          <form onSubmit={btnUpdateDeviceOnClick} className="space-y-4">
+            <div>
+              <label className="block text-[#ccccd6]">Name</label>
+              <input
+                value={deviceName}
+                onChange={(e) => setDeviceName(e.target.value)}
+                type="text"
+                className="mt-1 p-2 w-full border rounded-md bg-[#d3d5de]"
+                required
+              />
             </div>
-        </>
-    );
-    };
 
+            <div>
+              <label className="block text-[#ccccd6]">Username</label>
+              <input
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                type="text"
+                className="mt-1 p-2 w-full border rounded-md bg-[#d3d5de]"
+                required
+              />
+            </div>
 
-export default UpdateDeviceModal
+            <div>
+              <label className="block text-[#ccccd6]">Password</label>
+              <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+                className="mt-1 p-2 w-full border rounded-md bg-[#d3d5de]"
+                required
+              />
+            </div>
+
+            <div>
+              <label className="block text-[#ccccd6]">IP Address</label>
+              <input
+                value={ipAddress}
+                onChange={(e) => setIpAddress(e.target.value)}
+                type="text"
+                className="mt-1 p-2 w-full border rounded-md bg-[#d3d5de]"
+                required
+              />
+            </div>
+
+            <div className="flex justify-end space-x-2">
+              <button
+                type="button"
+                onClick={onClose}
+                className="px-6 py-2 rounded-md bg-[#1e1e2b] border-[#4b9eda] border-2 
+                  text-[#4b9eda] hover:bg-[#252c42] hover:text-[#4b9eda] hover:cursor-pointer"
+              >
+                Cancel
+              </button>
+
+              <button
+                type="submit"
+                className="px-8 py-2 rounded-md bg-[#1e1e2b] border-[#4b9eda] border-2 
+                  text-[#4b9eda] hover:bg-[#252c42] hover:text-[#4b9eda] hover:cursor-pointer"
+              >
+                Update
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default UpdateDeviceModal;
