004f0bc8da1ffc2f5a57a1fcbbaf240ec83c638e
AshikaDandeniya <ashikadandeniya@gmail.com>
2025-05-28 15:46:08 +0530

    Updated alert messages

--- src/components/AddDeviceModal.jsx
+++ src/components/AddDeviceModal.jsx
@@ -1,85 +1,129 @@
 import React, { useState } from "react";
+import { Alert, Slide } from "@mui/material";
 import { backendAddress } from "../constants/BackendInfo";
 
 const AddDeviceModal = ({ isOpen, onClose, onSubmit }) => {
   if (!isOpen) return null;
 
-  const[deviceName, setDeviceName] = useState("");
-  const[username, setUsername] = useState("");
-  const[password, setPassword] = useState("");
-  const[ipAddress, setIpAddress] = useState("");
+  const [deviceName, setDeviceName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [ipAddress, setIpAddress] = useState("");
+  const [showAlert, setShowAlert] = useState(false);
 
-  const btnAddDeviceOnClick = () => {
-    if (!isValidated()) return;
+  const btnAddDeviceOnClick = (e) => {
+    e.preventDefault();
+
     
+    setShowAlert(true);
+    setTimeout(() => setShowAlert(false), 3000); 
+
+    if (!isValidated()) return;
+
     fetch(`${backendAddress}/api/v1/device`, {
-        method: "POST",
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-        body: JSON.stringify({
-            name: deviceName,
-            username: username,
-            password: password,
-            ipAddress: ipAddress,
-        }),
+      method: "POST",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify({
+        name: deviceName,
+        username,
+        password,
+        ipAddress,
+      }),
     })
-    .then((response) => response.json())
-    .then(data => {
-        if(data.success){
-            alert("Device added successfully.")
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          onSubmit(); 
         }
-    });
-  }
+      });
+  };
 
   const isValidated = () => {
-    return true;
-  }
-
-
+    return true; 
+  };
 
   return (
-    <>
-        <div className="fixed inset-0 bg-opacity-40 backdrop-blur-md flex justify-center items-center z-10">
-            <div className="p-6 rounded-lg shadow-lg w-160 bg-[#1e1e2b]">
-                <h2 className="text-xl font-bold mb-4 text-[#d3d5de]">ADD DEVICE</h2>
+    <div className="fixed inset-0 bg-opacity-40 backdrop-blur-md flex justify-center items-center z-10">
+      <div className="relative p-6 rounded-lg shadow-lg w-160 bg-[#1e1e2b]">
 
-                <form onSubmit={btnAddDeviceOnClick} className="space-y-4">
-                    <div>
-                        <label className="block text-[#ccccd6]">Name</label>
-                        <input value={deviceName} onChange={e => setDeviceName(e.target.value)} type="text" className="mt-1 p-2 w-full border rounded-md bg-[#d3d5de]" required />
-                    </div>
-
-                    <div>
-                        <label className="block text-[#ccccd6]">Username</label>
-                        <input value={username} onChange={e => setUsername(e.target.value)} type="text" className="mt-1 p-2 w-full border rounded-md bg-[#d3d5de]" required />
-                    </div>
-
-                    <div>
-                        <label className="block text-[#ccccd6]">Password</label>
-                        <input value={password} onChange={e => setPassword(e.target.value)} type="password" className="mt-1 p-2 w-full border rounded-md bg-[#d3d5de]" required />
-                    </div>
-
-                    <div>
-                        <label className="block text-[#ccccd6]">IP Address</label>
-                        <input value={ipAddress} onChange={e => setIpAddress(e.target.value)} type="text" className="mt-1 p-2 w-full border rounded-md bg-[#d3d5de]" required />
-                    </div>
-
-                    <div className="flex justify-end space-x-2">
-                        <button type="button" onClick={onClose} className="px-6 py-2 rounded-md bg-[#1e1e2b] border-[#4b9eda] border-2 
-                            text-[#4b9eda] hover:bg-[#252c42] hover:text-[#4b9eda]  hover:cursor-pointer">
-                            Cancel
-                        </button>
-                
-                        <button type="submit" className="px-8 py-2 rounded-md bg-[#1e1e2b] border-[#4b9eda] border-2 
-                            text-[#4b9eda] hover:bg-[#252c42] hover:text-[#4b9eda]  hover:cursor-pointer">
-                            Add
-                        </button>
-                    </div>
-                </form>
+        
+        {showAlert && (
+          <Slide direction="down" in={showAlert} mountOnEnter unmountOnExit>
+            <div className="absolute top-4 z-50 w-full flex justify-center">
+              <Alert severity="info" className="w-1/2 text-center">
+                Device added successfully!
+              </Alert>
             </div>
-        </div>
-    </>
+          </Slide>
+        )}
+
+        <h2 className="text-xl font-bold mb-4 text-[#d3d5de]">ADD DEVICE</h2>
+
+        <form onSubmit={btnAddDeviceOnClick} className="space-y-4">
+          <div>
+            <label className="block text-[#ccccd6]">Name</label>
+            <input
+              value={deviceName}
+              onChange={(e) => setDeviceName(e.target.value)}
+              type="text"
+              className="mt-1 p-2 w-full border rounded-md bg-[#d3d5de]"
+              required
+            />
+          </div>
+
+          <div>
+            <label className="block text-[#ccccd6]">Username</label>
+            <input
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              type="text"
+              className="mt-1 p-2 w-full border rounded-md bg-[#d3d5de]"
+              required
+            />
+          </div>
+
+          <div>
+            <label className="block text-[#ccccd6]">Password</label>
+            <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              className="mt-1 p-2 w-full border rounded-md bg-[#d3d5de]"
+              required
+            />
+          </div>
+
+          <div>
+            <label className="block text-[#ccccd6]">IP Address</label>
+            <input
+              value={ipAddress}
+              onChange={(e) => setIpAddress(e.target.value)}
+              type="text"
+              className="mt-1 p-2 w-full border rounded-md bg-[#d3d5de]"
+              required
+            />
+          </div>
+
+          <div className="flex justify-end space-x-2">
+            <button
+              type="button"
+              onClick={onClose}
+              className="px-6 py-2 rounded-md bg-[#1e1e2b] border-[#4b9eda] border-2 text-[#4b9eda] hover:bg-[#252c42] hover:cursor-pointer"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="px-8 py-2 rounded-md bg-[#1e1e2b] border-[#4b9eda] border-2 text-[#4b9eda] hover:bg-[#252c42] hover:cursor-pointer"
+            >
+              Add
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 };
 
--- src/components/Firmware.jsx
+++ src/components/Firmware.jsx
@@ -1,110 +1,128 @@
-import { colors } from "@mui/material";
 import { useState } from "react";
 import { FaUpload } from "react-icons/fa";
-import Select from "react-select"; 
+import Select from "react-select";
+import { Alert, Slide } from "@mui/material";
 
 export default function FirmwareUpdate() {
-  const [step, setStep] = useState(1);
   const [firmware, setFirmware] = useState("");
   const [device, setDevice] = useState("");
   const [deviceDetails, setDeviceDetails] = useState({});
   const [file, setFile] = useState(null);
+  const [updateSuccess, setUpdateSuccess] = useState(false);
 
   const deviceOptions = [
     { label: "Device 1", value: "Device 1", ip: "192.168.1.10" },
     { label: "Device 2", value: "Device 2", ip: "192.168.1.20" },
     { label: "Device 3", value: "Device 3", ip: "192.168.1.30" },
-    { label: "Device 4", value: "Device 4", ip: "192.168.1.30" },
-    { label: "Device 45", value: "Device 45", ip: "192.168.1.30" },
+    { label: "Device 4", value: "Device 4", ip: "192.168.1.40" },
+    { label: "Device 45", value: "Device 45", ip: "192.168.1.50" },
   ];
 
   const handleDeviceChange = (selectedOption) => {
     setDevice(selectedOption.value);
-
-    
     const deviceInfo = deviceOptions.find((dev) => dev.value === selectedOption.value);
     setDeviceDetails(deviceInfo || {});
+    setUpdateSuccess(false);
   };
 
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
+    setUpdateSuccess(false);
   };
 
-  
+  const handleUpdate = () => {
+    if (device && file) {
+      setTimeout(() => {
+        setUpdateSuccess(true);
+        setTimeout(() => setUpdateSuccess(false), 3000); // Hide alert after 3 seconds
+      }, 1000);
+    } else {
+      alert("Please select a device and choose a firmware file.");
+    }
+  };
 
-  
-    const customStyles = {
-        control: (provided, state) => ({
-         ...provided,
-            backgroundColor: '#d3d5de',
-            color:'black',
-            border:'none',
-            boxShadow: 'none',
-            '&:hover': {
-            border: 'none',       
-            },
-        }),
-        placeholder: (provided) => ({
-         ...provided,
-            color: '#000',
-        }),
+  const customStyles = {
+    control: (provided) => ({
+      ...provided,
+      backgroundColor: '#d3d5de',
+      color: 'black',
+      border: 'none',
+      boxShadow: 'none',
+      '&:hover': {
+        border: 'none',
+      },
+    }),
+    placeholder: (provided) => ({
+      ...provided,
+      color: '#000',
+    }),
+    option: (provided, state) => ({
+      ...provided,
+      color: 'black',
+      backgroundColor: state.isSelected ? '#f1f1f1' : state.isFocused ? '#d1d1d1' : '#d3d5de',
+    }),
+  };
 
-         option: (provided, state) => ({
-         ...provided,
-            color:'black',
-            backgroundColor: state.isSelected ? '#f1f1f1' : state.isFocused ? '#d1d1d1' : '#d3d5de',
-        }),
-    };
+  return (
+    <div className="flex flex-col items-center min-h-screen bg-[#15151f] relative">
+      
+      {updateSuccess && (
+        <Slide direction="down" in={updateSuccess} mountOnEnter unmountOnExit>
+          <div className="absolute top-4 z-50 w-full flex justify-center">
+            <Alert severity="info" className="w-1/2 text-center">
+              Firmware updated successfully!
+            </Alert>
+          </div>
+        </Slide>
+      )}
 
-    return (
-        <div className="flex flex-col items-center min-h-screen bg-[#15151f]">
-            <div className="w-full max-w-3xl h-full bg-[#1e1e2b] mt-20">
-                <h2 className="text-4xl font-bold mt-10 text-center text-[#d3d5de]">
-                UPDATE FIRMWARE
-                </h2>
+      <div className="w-full max-w-3xl h-full bg-[#1e1e2b] mt-20">
+        <h2 className="text-4xl font-bold mt-10 text-center text-[#d3d5de]">
+          UPDATE FIRMWARE
+        </h2>
 
-                <div className="space-y-16">
-                    <div className="flex items-center justify-between">
-                        <Select
-                        className="flex-1 mx-6 pt-10 "
-                        options={deviceOptions}
-                        onChange={handleDeviceChange}
-                        placeholder="Select Device"
-                        getOptionLabel={(e) => e.label}
-                        getOptionValue={(e) => e.value}
-                        styles={customStyles} 
-                    />
-                </div>
+        <div className="space-y-16">
+          <div className="flex items-center justify-between">
+            <Select
+              className="flex-1 mx-6 pt-10"
+              options={deviceOptions}
+              onChange={handleDeviceChange}
+              placeholder="Select Device"
+              getOptionLabel={(e) => e.label}
+              getOptionValue={(e) => e.value}
+              styles={customStyles}
+            />
+          </div>
 
-                {device && deviceDetails.label && (
-                    <div className="flex-1 mx-6">
-                        <p className="text-[#d3d5de]">Device Name: {deviceDetails.label}</p>
-                        <p className="text-[#d3d5de]">IP Address: {deviceDetails.ip}</p>
-                    </div>
-                )}
-                <div className="flex items-center justify-between mt-6">
-                    <label className="flex-1 border p-3 rounded-md mx-6 cursor-pointer bg-[#d3d5de]">
-                        <div className="flex items-center justify-center space-x-3">
-                            <FaUpload className="text-gray-700" size={20} />
-                            <span className="text-black">{file ? file.name : "Choose Firmware File"}</span>
-                        </div>
-                        <input
-                            type="file"
-                            className="hidden"
-                            onChange={handleFileChange}
-                        />
-                    </label>
-                </div>
-                <div className="flex items-center justify-between mt-6">
-                    <button
-                        className="flex-1  bg-[#1e1e2b] border-[#4b9eda] border-2 text-[#4b9eda] mb-10 px-5 py-3 
-                        rounded-md mx-6 hover:bg-[#252c42] hover:text-[#4b9eda]  hover:cursor-pointer"
-                    >
-                        Update Device
-                    </button>
-                </div>
+          {device && deviceDetails.label && (
+            <div className="flex-1 mx-6">
+              <p className="text-[#d3d5de]">Device Name: {deviceDetails.label}</p>
+              <p className="text-[#d3d5de]">IP Address: {deviceDetails.ip}</p>
             </div>
+          )}
+
+          <div className="flex items-center justify-between mt-6">
+            <label className="flex-1 border p-3 rounded-md mx-6 cursor-pointer bg-[#d3d5de]">
+              <div className="flex items-center justify-center space-x-3">
+                <FaUpload className="text-gray-700" size={20} />
+                <span className="text-black">
+                  {file ? file.name : "Choose Firmware File"}
+                </span>
+              </div>
+              <input type="file" className="hidden" onChange={handleFileChange} />
+            </label>
+          </div>
+
+          <div className="flex items-center justify-between mt-6">
+            <button
+              onClick={handleUpdate}
+              className="flex-1 bg-[#1e1e2b] border-[#4b9eda] border-2 text-[#4b9eda] mb-4 px-5 py-3 rounded-md mx-6 hover:bg-[#252c42] hover:text-[#4b9eda] hover:cursor-pointer"
+            >
+              Update Device
+            </button>
+          </div>
         </div>
+      </div>
     </div>
   );
 }
